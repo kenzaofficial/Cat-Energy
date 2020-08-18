66912a138a8ea39934299a5b0fcee9e9a96538e4
kenzaofficial <yuraha15@yandex.ru>
2020-08-18 13:35:12 +0300

    latest gulp-set valid

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let path = {
       fonts: project_folder + "/fonts/",
    },
    src: {
-      html: source_folder + "/*.html",
+      html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
       css: source_folder + "/scss/style.scss",
       js: source_folder + "/js/js.js",
       img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
@@ -25,9 +25,22 @@ let path = {
    clean: "./" + project_folder + "/"
 }
 
-let { src, dest } = require('gulp'),
-gulp = require ('gulp'),
-browsersyns = require("browser-sync").create();
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browsersyns = require("browser-sync").create(),
+  fileinclude = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer"),
+  group_media = require("gulp-group-css-media-queries"),
+  clean_css = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require("gulp-webp"),
+ // webphtml = require('gulp-webp-html'); на этом шаге перестало работать
+    
+  
 
 function browserSyns(params) {
    browsersyns.init({
@@ -41,13 +54,90 @@ function browserSyns(params) {
 
 function html() {
    return src(path.src.html)
-      .pipe(dest(path.build.html))
-      .pipe(browsersyns.stream())
+     .pipe(fileinclude())
+     //.pipe(webphtml()) //здесь перестает работать.
+     .pipe(dest(path.build.html))
+     .pipe(browsersyns.stream())
 }
 
-let build = gulp.series(html);
-let watch = gulp.parallel(browserSyns);
+function css() {
+   return src(path.src.css)
+     .pipe(
+       scss({
+         outputStyle: "expanded",
+       })
+     )
+     .pipe(group_media())
+     .pipe(
+       autoprefixer({
+         overrideBrowserslist: ["last 5 versions"],
+         cascade: true,
+       })
+     )
+     .pipe(dest(path.build.css))
+     .pipe(clean_css())
+     .pipe(
+       rename({
+         extname: ".min.css",
+       })
+     )
+     .pipe(dest(path.build.css))
+     .pipe(browsersyns.stream())
+     
+}
 
+function js() {
+   return src(path.src.js)
+     .pipe(fileinclude())
+     .pipe(dest(path.build.js))
+     .pipe(uglify())
+     .pipe(
+       rename({
+         extname: ".min.js",
+       })
+     )
+     .pipe(dest(path.build.js))
+     .pipe(browsersyns.stream())
+}
+
+function images() {
+  return src(path.src.img)
+   .pipe(
+     webp({
+         quality: 70
+    })
+  )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+         imagemin({
+           progressive: true,
+           svgoPlugins: [{ removeViewBox: false }],
+           interlaced: true,
+           optimizationLevel: 3, // 0 to 7
+         })
+       )
+       .pipe(dest(path.build.img))
+       .pipe(browsersyns.stream())
+}
+
+function watchFiles(params) {
+   gulp.watch([path.watch.html], html);
+   gulp.watch([path.watch.css], css);
+   gulp.watch([path.watch.js], js);
+   gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+   return del(path.clean)
+}
+
+let build = gulp.series(clean, gulp.parallel(js,css, html, images));
+let watch = gulp.parallel(build, watchFiles, browserSyns);
+
+exports.images = images;
+exports.js = js;
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
